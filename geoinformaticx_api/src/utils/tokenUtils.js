7614ae7a8713e96ce46d84bcf7b3dc5392a44859
Sourav Palit <environment@geomaticx.com>
2026-09-21 10:30:39 +0530

    Fix cross-site auth cookie

--- geoinformaticx_api/src/utils/tokenUtils.js
+++ geoinformaticx_api/src/utils/tokenUtils.js
@@ -11,7 +11,7 @@ exports.setAuthCookie = (res, token) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
@@ -27,7 +27,7 @@ exports.setCustomerAuthCookie = (res, token) => {
   res.cookie('customer_token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: 'none',
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
